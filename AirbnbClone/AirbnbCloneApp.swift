42c7fa7f98f0a5fddc0e05ceac567a7fc463dd86
Pranav R <pranav81234@gmail.com>
2024-03-31 21:48:16 +0530

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Pranav R on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
